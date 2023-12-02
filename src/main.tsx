5b41fd0e244185967b4f912a6d6e14785f576a82
KANG DONGHA <knamuuu@naver.com>
2023-12-03 00:03:30 +0900

    Design: global design 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
 
 import App from './App.tsx';
 
-import './index.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <RecoilRoot>
-                <App />
-            </RecoilRoot>
+            <App />
         </BrowserRouter>
     </React.StrictMode>
 );
