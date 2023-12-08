3f7c74639028dd879cc39781330b52d2fd5d4646
KANG DONGHA <knamuuu@naver.com>
2023-12-08 19:56:16 +0900

    Design: 메인페이지

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,10 @@
+import styles from './styles.module.scss';
+
 export default function Home() {
-    return <div>Home</div>;
+    return (
+        <div className={styles.container}>
+            <h1>여러분들을 위한 라이브 스트리밍 SaaS 플랫폼</h1>
+            <span>IN-STREAM</span>
+        </div>
+    );
 }
