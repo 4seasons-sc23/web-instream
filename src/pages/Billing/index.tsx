b46547023fe740d430c9abe1bbfdb67c26416efd
KANG DONGHA <knamuuu@naver.com>
2023-12-08 17:47:54 +0900

    Fix: build error 해결(사용되지 않는 react 제거)

--- src/pages/Billing/index.tsx
+++ src/pages/Billing/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { isAxiosError } from 'axios';
 
 import PaginationComponent from 'components/organisms/Common/Pagination';
